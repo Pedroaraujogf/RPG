ae59732834302b4cc806f3cfc85bc86af5eba4de
Pedroaraujogf <pedro_araujo1@hotmail.com>
2024-04-12 14:56:33 -0300

    Banco de dados

--- contador.py
+++ contador.py
@@ -1,4 +1,5 @@
 import flet as ft
+import sqlite3
 
 class Contador:
     def __init__(self,page: ft.Page):
@@ -9,7 +10,17 @@ class Contador:
         self.page.window_resizable = False
         self.page.window_always_on_top = True
         self.page.title = 'Contador App'
+        self.task = ""
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name, status)')
+        self.results = self.db_execute('SELECT * FROM tasks')
         self.main_page()
+    
+    def db_execute(sel, query, params = []):
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute(query, params)
+            con.commit()
+            return cur.fetchall()
 
     def tasks_container(self):
         return ft.Container(
@@ -17,19 +28,42 @@ class Contador:
             content = ft.Column(
                 controls = [
                     ft.TextField(
-                        hint_text='HP', label='Inimigo 1', width= 0.25
-                    )
+                        # Revisar esssa parte, estão funcionando mas tem erros.
+                        hint_text='HP', label=res[0], value = True if res[1] == 'complete' else False)
+                    for res in self.results if res
                 ]
             )
         )
+    
+    def set_value(self, e):
+        self.task = e.control.value
+        print(self.task)
+
+    def add(self, e, input_task):
+        name = self.task
+        status = 'incomplete'
+
+        if name:
+            self.db_execute(query='INSERT INTO tasks VALUES(?,?)', params=[name, status])
+            input_task.value=''
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.update_task_list()
+    
+    def update_task_list(self):
+        tasks = self.tasks_container()
+        self.page.controls.pop()
+        self.page.add(tasks)
+        self.page.updade()
 
     def main_page(self):
-        input_task = ft.TextField(hint_text = 'Digite os nomes', expand=True)
+        input_task = ft.TextField(hint_text = 'Digite os nomes', expand=True, on_change=self.set_value)
 
         input_bar = ft.Row(
             controls= [
                 input_task,
-                ft.FloatingActionButton(icon=ft.icons.ADD)
+                ft.FloatingActionButton(icon=ft.icons.ADD,
+                                        on_click=lambda e: self.add(e, input_task)
+                )
             ]
         )
 
